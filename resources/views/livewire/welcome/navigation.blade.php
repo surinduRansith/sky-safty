ae0a42f59467dda158479387fb5a1c1e1ad5ec87
surinduRansith <surinduransith@gmail.com>
2025-03-27 16:34:51 +0530

    updateregister

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -15,13 +15,13 @@
         </a>
         <br>
 
-        {{-- @if (Route::has('register'))
+        @if (Route::has('register'))
             <a
                 href="{{ route('register') }}"
                 class=" btn-outline btn-primary rounded-md px-3 gap-4 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
             >
                 Register
             </a>
-        @endif --}}
+        @endif
     @endauth
 </nav>
